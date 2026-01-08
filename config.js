53cc6ef999ac13223a85239ec9877c85c496fbfb
aiysaaner22087-commits <aiysaaner9@gmail.com>
2026-01-08 07:26:02 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://XXXX.supabase.co",
-  SUPABASE_ANON_KEY: "ISI_ANON_KEY_KAMU"
+  SUPABASE_URL: "https://gijaliijrqthzoaiiutw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpamFsaWlqcnF0aHpvYWlpdXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4MjYxNzksImV4cCI6MjA4MzQwMjE3OX0.C9nIUYQNUMOepSJdSxOjyJwF_4gBOaMZo1kqH8XO9E4"
 };
